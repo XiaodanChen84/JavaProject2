ab3a5981eb9791292d95a930cd430a682d4c1df2
Xiaodan <chen0823@algonquinlive.com>
2021-04-11 15:15:39 -0400

    Merge origin/master
    Conflicts:
    	src/main/java/logic/PersonLogic.java

--- src/test/java/logic/PersonLogicTest.java
+++ src/test/java/logic/PersonLogicTest.java
@@ -26,7 +26,7 @@ import org.junit.jupiter.api.Test;
  *
  * @author xiaod
  */
-class PersonLogicTest1 {
+class PersonLogicTest {
     private PersonLogic logic;
     private Person expectedEntity;
     
