9db25e300802f0a1ce4c860bf2e7a7b401124a20
DanpingTang <77473578+DanpingTang@users.noreply.github.com>
2021-04-14 15:14:19 -0400

    updated

--- src/main/java/logic/LogicFactory.java
+++ src/main/java/logic/LogicFactory.java
@@ -15,7 +15,6 @@ public abstract class LogicFactory {
 
     //TODO this code is not complete, it is just here for sake of programe working. need to be changed ocmpletely
     public static < T> T getFor(String entityName) {
-//        T newInstance = null;
         try {
             T newInstance = getFor((Class< T>) Class.forName(PACKAGE + entityName + SUFFIX));
              return newInstance;
@@ -26,14 +25,15 @@ public abstract class LogicFactory {
     }
 
     public static <T> T getFor(Class<T> type) {
-        T newInstance = null;
+    
         try {
             Constructor<T> declaredConstructor = type.getDeclaredConstructor();
-            newInstance = declaredConstructor.newInstance();
+            T newInstance = declaredConstructor.newInstance();
+            return newInstance;
 
         } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
             Logger.getLogger(LogicFactory.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return newInstance;
+        return null;
     }
 }
